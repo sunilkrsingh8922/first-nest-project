db54b1ec21e5736d993c3b2e27fbf7cdb8a52ebb
SunilKgsApp <sunil.s@kgs.live>
2025-07-02 18:53:55 +0530

    bug

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000, '0.0.0.0');
+  Logger.log("Application run port" + 3000)
   console.log("hi now you");
 }
 bootstrap();
