e51b0a44e7888992ba88ad61b4e004159ed9c4e6
SunilKgsApp <sunil.s@kgs.live>
2025-07-02 16:27:53 +0530

    port is 22

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  const port=22;
+  Logger.log("app is running on port " + port)
+  await app.listen(port);
   console.log("hi now you");
 }
 bootstrap();
