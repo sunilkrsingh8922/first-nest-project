bb2ab9eb41df5d001962c1bea8e0dc3491968a83
SunilKgsApp <sunil.s@kgs.live>
2025-07-03 18:08:45 +0530

    callback function added

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ async function bootstrap() {
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000,'0.0.0.0');
+  await app.listen(3000,()=>{
+    console.log('running on port 3000')
+  });
 }
 
 bootstrap();
