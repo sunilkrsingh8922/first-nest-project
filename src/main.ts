645773bb71e90127411c1e4077d4036677d69a02
sunil kr singh <68856385+sunilkrsingh8922@users.noreply.github.com>
2025-07-04 22:46:02 +0530

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('My API')
-  //   .setDescription('API docs')
-  //   .setVersion('1.0')
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for my project')
+    .setVersion('1.0')
+    .addBearerAuth() // optional: if you're using JWT auth
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000,()=>{
+   await app.listen(3000,()=>{
     console.log('running on port 3000')
   });
 }
-
 bootstrap();
