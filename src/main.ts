e315822f3cc0b6bb1376e0774b05090c0177c1a4
SunilKgsApp <sunil.s@kgs.live>
2025-07-05 11:34:17 +0530

    Swager issue solved

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+   console.log(`🚀 Server is running on: http://localhost:${port}`);
+  console.log(`📚 Swagger docs available at: http://localhost:${port}/api`);
 }
 bootstrap();
